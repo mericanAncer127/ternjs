ternjs/tern

commit d4c72638d4dc3c37faff980b5cf1081dba8a7433

Fix bug in determining the scope inside a catch clause
Closes #134

lib/infer.js
@@ -1368,8 +1368,8 @@
   // LOCAL-VARIABLE QUERIES
 
   var scopeAt = exports.scopeAt = function(ast, pos, defaultScope) {
-    var found = walk.findNodeAround(ast, pos, "ScopeBody");
-    if (found) return found.node.scope;
+    var found = walk.findNodeAround(ast, pos, "Function");
+    if (found) return found.node.body.scope;
     else return defaultScope || cx.topScope;
   };
 

mericanAncer127/ternjs

commit c2fada680bf25ec79eedd567c4565142d084c1dc

Fix test failures introduced by d4c72638d4dc3c37faff980b5cf1081dba8a7433

lib/infer.js
@@ -1368,8 +1368,10 @@
   // LOCAL-VARIABLE QUERIES
 
   var scopeAt = exports.scopeAt = function(ast, pos, defaultScope) {
-    var found = walk.findNodeAround(ast, pos, "Function");
-    if (found) return found.node.body.scope;
+    var found = walk.findNodeAround(ast, pos, function(tp, node) {
+      return tp == "ScopeBody" && node.scope;
+    });
+    if (found) return found.node.scope;
     else return defaultScope || cx.topScope;
   };
 